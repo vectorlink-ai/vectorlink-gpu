e3ebdee9b7a82bcb80d386ca6d2e56af80eaa2d7
Maren van Otterdijk <maren@vectorlink.ai>
2024-11-29 14:56:31 +0100

    search algorithm

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub struct SearchParams {
     circulant_parameter_count: usize,
     parallel_visit_count: usize,
     visit_queue_size: usize,
+    search_queue_size: usize,
 }
 
 // Returns the number of candidates we are searching simultaneously.
@@ -66,6 +67,46 @@ pub fn closest_vectors(
 
         seen = Tensor::concat(&[seen, masked_indices], 0);
     }
+}
+
+pub fn search_layers(
+    layers: &[Tensor],
+    query_vec: &Tensor,
+    search_queue: &mut Queue,
+    vectors: &Tensor,
+    search_params: &SearchParams,
+) {
+    for layer in layers.iter() {
+        closest_vectors(query_vec, search_queue, vectors, layer, search_params);
+    }
+}
+
+pub fn search_from_initial(
+    layers: &[Tensor],
+    query_vec: &Tensor,
+    vectors: &Tensor,
+    search_params: &SearchParams,
+) -> Queue {
+    let n = std::cmp::min(
+        layers[0].size1().unwrap(),
+        search_params.parallel_visit_count as i64,
+    );
+    let initial_indices = Tensor::arange(n, (Kind::Int, query_vec.device()));
+    let initial_vectors = vectors.index_select(0, &initial_indices);
+    let initial_distances = comparison(&initial_vectors, query_vec);
+
+    // assumes that last neighborhood is the biggest
+    let (_, last_neighborhood_size) = layers.last().unwrap().size2().unwrap();
+
+    let extra_capacity = last_neighborhood_size * search_params.parallel_visit_count as i64;
+    let capacity = search_params.search_queue_size as i64 + extra_capacity;
+
+    let mut search_queue = Queue::new(search_params.search_queue_size as i64, capacity);
+    search_queue.insert(&initial_indices, &initial_distances);
+
+    search_layers(layers, query_vec, &mut search_queue, vectors, search_params);
+
+    search_queue
 }
 
 fn main() {
