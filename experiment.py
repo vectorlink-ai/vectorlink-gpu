2e68105057f6a3f9ddde931f063a7a75ddf34506
Gavin Mendel-Gleason <gavin@vectorlink.ai>
2024-12-19 14:31:08 +0000

    Revert "Shuffle magic"
    This reverts commit 6868789c2e8e60c77113c193a453c4f8eb206d53.

--- experiment.py
+++ experiment.py
@@ -140,21 +140,15 @@ class Queue:
         return self.indices.size()
 
 
-FULL_MASK = 0xFFFFFFFF
-
-
-@cuda.jit(float32(float32[::1], int64), device=True, inline=True, fastmath=True)
+@cuda.jit(void(float32[::1], int64), device=True, inline=True, fastmath=True)
 def warp_reduce(vec, thread_idx):
-    val = 0.0
     if thread_idx < 32:
-        val = vec[thread_idx]
-        val += numba.cuda.shfl_down_sync(FULL_MASK, val, 32)
-        val += numba.cuda.shfl_down_sync(FULL_MASK, val, 16)
-        val += numba.cuda.shfl_down_sync(FULL_MASK, val, 8)
-        val += numba.cuda.shfl_down_sync(FULL_MASK, val, 4)
-        val += numba.cuda.shfl_down_sync(FULL_MASK, val, 2)
-        val += numba.cuda.shfl_down_sync(FULL_MASK, val, 1)
-    return val
+        vec[thread_idx] += vec[thread_idx + 32]
+        vec[thread_idx] += vec[thread_idx + 16]
+        vec[thread_idx] += vec[thread_idx + 8]
+        vec[thread_idx] += vec[thread_idx + 4]
+        vec[thread_idx] += vec[thread_idx + 2]
+        vec[thread_idx] += vec[thread_idx + 1]
 
 
 @cuda.jit(float32(float32[::1], int64, int64), device=True, inline=True, fastmath=True)
@@ -190,10 +184,8 @@ def sum(vec, dim, idx):
         return 0.0
     numba.cuda.syncthreads()
 
-    return warp_reduce(vec, idx)
-
-
-#    return vec[0]
+    warp_reduce(vec, idx)
+    return vec[0]
 
 
 @cuda.jit(void(float32[::1], float32[::1]))
