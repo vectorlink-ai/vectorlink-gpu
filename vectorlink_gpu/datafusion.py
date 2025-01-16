cf85680868a587d7a763ce3ea1d6b8fe20f7b63a
Maren van Otterdijk <maren@vectorlink.ai>
2025-01-16 17:09:09 +0100

    dataframe_to_tensor now works for int32 and float32 and different sizes

--- vectorlink_gpu/datafusion.py
+++ vectorlink_gpu/datafusion.py
@@ -24,19 +24,29 @@ def build_local_session_context(directory: str) -> SessionContext:
     return context
 
 
+def dtype_to_ctype(dtype):
+    if dtype == torch.float32:
+        return ctypes.c_float
+    elif dtype == torch.int32:
+        return ctypes.c_int32
+    else:
+        raise ValueError(f"i don't know what a {dtype} is")
+
+
 def dataframe_to_tensor(df: DataFrame, tensor: Tensor):
     # assuming 1536 length floats. this needs to be more properly done later
     # also assuming that the incoming dataframe is sorted!
     cuda_array = cuda.as_cuda_array(tensor)
     stream = df.execute_stream()
     offset = 0
+    ctype = dtype_to_ctype(tensor.dtype)
     for batch in stream:
         embeddings = batch.to_pyarrow().column(0)
         embeddings_ptr = ctypes.cast(
-            embeddings.buffers()[3].address, ctypes.POINTER(ctypes.c_float)
+            embeddings.buffers()[3].address, ctypes.POINTER(ctype)
         )
         embeddings_numpy = np.ctypeslib.as_array(
-            embeddings_ptr, (len(embeddings), 1536)
+            embeddings_ptr, (len(embeddings), tensor.size()[1])
         )
         cuda_slice = cuda_array[offset : offset + len(embeddings)]
         cuda_slice.copy_to_device(embeddings_numpy)
